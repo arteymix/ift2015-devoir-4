ff3391a3068c2527228bccddcda25da8bedfb993
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2014-12-13 14:34:47 -0500

    Inclut le titre dans les termes d'un document.

--- tp4/documents.py
+++ tp4/documents.py
@@ -9,11 +9,6 @@ from pkgutil import get_data
 # utilise un parser « safe »
 parser = XMLParser(recover=True)
 
-def get_reuters_stopwords():
-    """Retourne une liste de stopwords"""
-    root = parse(os.path.join(os.path.dirname(__file__), 'data/stopwords.xml'), parser)
-    return [word.text for word in root.iter('word')]
-
 class Document:
     """Représente un document"""
     def __init__(self, title, body):
@@ -23,11 +18,17 @@ class Document:
     def terms(self):
         """
         Retourne les termes du document sous forme d'un Counter en excluant les
-        stopwords
+        stopwords.
         """
-        terms = re.findall(r"[\w']+", self.body)
+        terms = re.findall(r"[\w']+", ' '.join([self.title, self.body]))
         stopwords = get_reuters_stopwords()
         return Counter([term for term in terms if term not in stopwords])
+
+def get_reuters_stopwords():
+    """Retourne une liste de stopwords"""
+    root = parse(os.path.join(os.path.dirname(__file__), 'data/stopwords.xml'), parser)
+    return [word.text for word in root.iter('word')]
+
 
 def get_reuters_documents():
     """Fetch all Reuteurs document and return a list of Document objects."""
